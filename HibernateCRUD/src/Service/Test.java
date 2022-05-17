c601863f40b3934b97bd6b42018e311f249c9368
Jeyachithra Balaraman <98647655+JeyachithraBalaraman@users.noreply.github.com>
2022-05-17 20:04:43 +0530

    Add files via upload
    Hibernate- errors

---
+++ HibernateCRUD/src/Service/Test.java
@@ -0,0 +1,12 @@
+package Service;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+public class Test {
+ public static void main(String[] args) {
+	System.out.println("Connecting to database");
+	SessionFactory factory=new Configuration()
+			.configure("hibernate.cfg.xml")
+			.buildSessionFactory();
+	System.out.println("Created");
+}
+}
